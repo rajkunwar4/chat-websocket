82dd3d3d115691a7139262add2b405446b3a370c
rajkunwar4 <40rajkunwar@gmail.com>
2025-02-21 06:08:53 +0530

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT,() => {
+  console.log(`server running on port ${PORT}`);
+});
+
+app.use(express.static(path.join(__dirname, 'public')))
+
